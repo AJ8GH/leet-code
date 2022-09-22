b639a7ce021b6f389d47523eef5f5c68398a2d7a
Adam Jonas <adam.jonas@ppb.com>
2022-09-22 22:15:28 +0100

    Add algo day 4 problems

---
+++ tracks/algorithms/src/main/java/io/github/aj8gh/leetcode/algo/track1/day4/Reverser.java
@@ -0,0 +1,8 @@
+package io.github.aj8gh.leetcode.algo.track1.day4;
+
+public class Reverser {
+
+  public void reverseString(char[] s) {
+
+  }
+}
---
+++ tracks/algorithms/src/main/java/io/github/aj8gh/leetcode/algo/track1/day4/WordReverser.java
@@ -0,0 +1,7 @@
+package io.github.aj8gh.leetcode.algo.track1.day4;
+
+public class WordReverser {
+  public String reverseWords(String s) {
+    return null;
+  }
+}
