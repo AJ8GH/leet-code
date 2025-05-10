e9e61cb9e8c79c293cbc9bcd8a96cc08408a2d9f
Adam Jonas <adamjonas25@gmail.com>
2025-05-10 20:57:57 +0100

    Solve products of array except self

--- puzzles/src/main/java/io/github/aj8gh/leetcode/blind75/medium/encodeanddecodestrings/Solution.java
+++ puzzles/src/main/java/io/github/aj8gh/leetcode/blind75/medium/encodeanddecodestrings/Solution.java
@@ -1,14 +1,24 @@
 package io.github.aj8gh.leetcode.blind75.medium.encodeanddecodestrings;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Solution {
 
+  private static final String DELIM = "_____DELIM_____";
+
   public String encode(List<String> strs) {
-    return "";
+    if (strs.isEmpty()) {
+      return null;
+    }
+    return String.join(DELIM, strs);
   }
 
   public List<String> decode(String str) {
-    return List.of();
+    if (str == null) {
+      return Collections.emptyList();
+    }
+    return Arrays.asList(str.split(DELIM));
   }
 }
--- puzzles/src/test/java/io/github/aj8gh/leetcode/blind75/medium/productsofarrayexceptself/SolutionTest.java
+++ puzzles/src/test/java/io/github/aj8gh/leetcode/blind75/medium/productsofarrayexceptself/SolutionTest.java
@@ -1,8 +1,7 @@
-package io.github.aj8gh.leetcode.blind75.medium.encodeanddecodestrings;
+package io.github.aj8gh.leetcode.blind75.medium.productsofarrayexceptself;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -14,16 +13,15 @@ class SolutionTest {
 
   private static Stream<Arguments> scenarios() {
     return Stream.of(
-        Arguments.of(List.of("neet", "code", "love", "you")),
-        Arguments.of(List.of("we", "say", ":", "yes")),
-        Arguments.of(List.of()));
+        Arguments.of(new int[] {1, 2, 4, 6}, new int[] {48, 24, 12, 8}),
+        Arguments.of(new int[] {-1, 0, 1, 2, 0}, new int[] {0, 0, 0, 0, 0}),
+        Arguments.of(new int[] {-1, 0, 1, 2, 3}, new int[] {0, -6, 0, 0, 0}));
   }
 
   @ParameterizedTest
   @MethodSource("scenarios")
-  void test(List<String> strs) {
-    var encoded = subject.encode(strs);
-    var actual = subject.decode(encoded);
-    assertThat(actual).isEqualTo(strs);
+  void test(int[] nums, int[] expected) {
+    var actual = subject.solve(nums);
+    assertThat(actual).isEqualTo(expected);
   }
 }
