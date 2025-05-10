e2b40c515651a0dda801fe4a328daa8b0efe5c7d
Adam Jonas <adamjonas25@gmail.com>
2025-05-10 17:22:06 +0100

    Solve contains duplicate

---
+++ puzzles/src/test/java/io/github/aj8gh/leetcode/blind75/easy/containsduplicate/SolutionTest.java
@@ -0,0 +1,26 @@
+package io.github.aj8gh.leetcode.blind75.easy.containsduplicate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class SolutionTest {
+
+  private final Solution subject = new Solution();
+
+  private static Stream<Arguments> scenarios() {
+    return Stream.of(
+        Arguments.of(new int[] {1, 2, 3, 4}, false),
+        Arguments.of(new int[] {1, 2, 3, 3}, true));
+  }
+
+  @ParameterizedTest
+  @MethodSource("scenarios")
+  void hasDuplicate(int[] nums, boolean expected) {
+    var actual = subject.hasDuplicate(nums);
+    assertThat(actual).isEqualTo(expected);
+  }
+}
