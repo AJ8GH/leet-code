f54a95ee71cf3b5bd909736d7478384d7506ca33
Adam Jonas <adamjonas25@gmail.com>
2025-05-11 10:46:09 +0100

    Refactor top k frequent with bucket sort

--- puzzles/src/main/java/io/github/aj8gh/leetcode/blind75/medium/longestconsecutivesequence/Solution.java
+++ puzzles/src/main/java/io/github/aj8gh/leetcode/blind75/medium/longestconsecutivesequence/Solution.java
@@ -1,8 +1,33 @@
 package io.github.aj8gh.leetcode.blind75.medium.longestconsecutivesequence;
 
+import java.util.TreeSet;
+
 public class Solution {
 
+  public record Range(
+      int high,
+      int low
+  ) {
+
+  }
+
   public int solve(int[] nums) {
+    // [2,20,4,10,3,4,5]
+    // [2, 2]
+    // [2, 2] [20, 20]
+    // [2, 2] [20, 20] [4, 4]
+    // [2, 2] [20, 20] [4, 4] [10, 10]
+    // [2, 3] [20, 20] [4, 4] [10, 10]
+    // [2, 3] [20, 20] [4, 4] [10, 10]
+    // [2, 3, 4] [20, 20] [4, 4] [10, 10]
+    // [2, 3, 4, 5] [20, 20] [4, 4] [10, 10]
+
+    //var ranges = TreeSet<Integer>
+    for (var n : nums) {
+
+
+    }
+
     return 0;
   }
 }
--- puzzles/src/main/java/io/github/aj8gh/leetcode/blind75/medium/topkfrequentelements/Solution.java
+++ puzzles/src/main/java/io/github/aj8gh/leetcode/blind75/medium/topkfrequentelements/Solution.java
@@ -1,22 +1,37 @@
 package io.github.aj8gh.leetcode.blind75.medium.topkfrequentelements;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Map.Entry;
+import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Solution {
 
   public int[] solve(int[] nums, int k) {
-    return Arrays.stream(nums)
+    var bucketSort = new ArrayList<List<Integer>>();
+
+    IntStream
+        .range(0, nums.length)
+        .forEach(n -> bucketSort.add(new ArrayList<>()));
+
+    Arrays.stream(nums)
         .boxed()
         .collect(Collectors.groupingBy(key -> key, Collectors.counting()))
-        .entrySet()
-        .stream()
-        .sorted(Entry.comparingByValue(Comparator.reverseOrder()))
-        .map(Entry::getKey)
-        .limit(k)
-        .mapToInt(Integer::intValue)
-        .toArray();
+        .forEach((key, value) -> bucketSort.get(value.intValue() - 1).add(key));
+
+    var out = new int[k];
+    for (int i = nums.length - 1; i >= 0; i--) {
+      var group = bucketSort.get(i);
+      for (var val : group) {
+        k--;
+        out[k] = val;
+        if (k == 0) {
+          return out;
+        }
+      }
+    }
+
+    return out;
   }
 }
