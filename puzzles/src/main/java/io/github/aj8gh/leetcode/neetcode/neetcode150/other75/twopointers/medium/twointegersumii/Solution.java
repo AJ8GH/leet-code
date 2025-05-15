3d4161b56d897c3fac90d17eca42e5b284a4121c
Adam Jonas <adamjonas25@gmail.com>
2025-05-15 22:23:05 +0100

    Refactor two sum II

--- puzzles/src/main/java/io/github/aj8gh/leetcode/neetcode/neetcode150/other75/twopointers/medium/twointegersumii/Solution.java
+++ puzzles/src/main/java/io/github/aj8gh/leetcode/neetcode/neetcode150/other75/twopointers/medium/twointegersumii/Solution.java
@@ -3,59 +3,24 @@ package io.github.aj8gh.leetcode.neetcode.neetcode150.other75.twopointers.medium
 public class Solution {
 
   public int[] solve(int[] numbers, int target) {
+    var i = 0;
+    var j = numbers.length - 1;
 
-    /*
+    while (i < j) {
+      var sum = numbers[i] + numbers[j];
 
-      [1, 2, 3, 4] 3
-      [1, 2]
-
-      [1, 3, 4, 7, 8, 10, 12, 15] 14
-      [4, 10]
-
-     */
-
-    for (int i = 0; i < numbers.length; i++) {
-      var n = numbers[i];
-      var diff = target - n;
-
-      if (diff > n) {
-        var found = find(numbers, i+1, numbers.length - 1, diff);
-        if (found >= 0) {
-          return new int[] {i + 1, found + 1};
-        }
+      while (sum > target) {
+        sum = numbers[i] + numbers[--j];
       }
 
-      if (diff < n) {
-        var found = find(numbers, 0, i - 1, diff);
-        if (found >= 0) {
-          return new int[] {i + 1, found + 1};
-        }
+      while (sum < target) {
+        sum = numbers[++i] + numbers[j];
+      }
+
+      if (sum == target) {
+        return new int[] {i + 1, j + 1};
       }
     }
-
     return numbers;
-  }
-
-  private int find(int[] nums, int i, int j, int target) {
-    while (j - i > -1) {
-      if (nums[i] == target) {
-        return i;
-      }
-      if (nums[j] == target) {
-        return j;
-      }
-      var index = (j - i) / 2 + i;
-      var result = nums[index];
-      if (result == target) {
-        return index;
-      }
-
-      if (result > target) {
-        i = index + 1;
-      } else {
-        j = index - 1;
-      }
-    }
-    return -1;
   }
 }
--- puzzles/src/test/java/io/github/aj8gh/leetcode/neetcode/neetcode150/other75/twopointers/medium/twointegersumii/SolutionTest.java
+++ puzzles/src/test/java/io/github/aj8gh/leetcode/neetcode/neetcode150/other75/twopointers/medium/twointegersumii/SolutionTest.java
@@ -15,7 +15,10 @@ class SolutionTest {
     return Stream.of(
         Arguments.of(new int[] {1, 2, 3, 4}, 3, new int[] {1, 2}),
         Arguments.of(new int[] {1, 3, 4, 7, 8, 10, 12, 15}, 14, new int[] {3, 6}),
-        Arguments.of(new int[] {-10, -2, 0, 1, 2, 4, 7, 8, 10, 12, 18}, 16, new int[] {2, 11}));
+        Arguments.of(new int[] {-10, -2, 0, 1, 2, 4, 7, 8, 10, 12, 18}, 16, new int[] {2, 11}),
+        Arguments.of(new int[] {-1000, -999, 0, 999, 1000}, -1, new int[] {1, 4}),
+        Arguments.of(new int[] {-10, -5, 0, 3, 7}, -2, new int[] {2, 4}),
+        Arguments.of(new int[] {1, 2, 5, 7, 9}, 12, new int[] {3, 4}));
   }
 
   @ParameterizedTest
